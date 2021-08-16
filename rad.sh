73c0d6bf58835ac22e1769714846476d4da6561f
zebusen <79355186+zebusen@users.noreply.github.com>
2021-08-16 16:13:54 +0800

    Update rad.sh

--- rad.sh
+++ rad.sh
@@ -125,7 +125,7 @@ ZIP=$(echo *.zip)
 }
 function sendInfo() {
 suffix="_defconfig"
-TYPE="$({CONFIG} | sed -e 's/^$suffix$//g')"
+TYPE=$(echo "$CONFIG" | sed -e 's/^$suffix$//g')
 curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
         -d chat_id="-1001214166550" \
         -d "disable_web_page_preview=true" \
