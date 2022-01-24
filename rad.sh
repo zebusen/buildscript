02c6131c7bfa5622f0331ea3f1e529e2c38c9442
zebusen <79355186+zebusen@users.noreply.github.com>
2022-01-24 15:04:00 +0800

    Fix aosp clang

--- rad.sh
+++ rad.sh
@@ -58,7 +58,8 @@ mkdir clang
     # wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r416183b.tar.gz
     # wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r428724.tar.gz
     # wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/c3260b409f13d92f8c9f4795420238694c529352/clang-r416183c1.tar.gz
-    wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/ecd70fc88e56db2ebcaa0a4d893e7c416eee84a8.tar.gz
+    # wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/ecd70fc88e56db2ebcaa0a4d893e7c416eee84a8.tar.gz
+    wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/9a12fa7b5a8a4b796ae34d04402151c64aeb6ad6/clang-r437112b.tar.gz
     tar -xzf clang*
     cd .. || exit
 cd ${WD}
